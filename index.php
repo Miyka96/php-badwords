94afe782b632e7c6b37a259f2d3aa0d2f0c2ba34
Miyka96 <micaelaseminara@outlook.it>
2022-03-28 16:21:56 +0200

    update

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-$parola="popipopi";
 
+$testo= "Metti la cera, togli la cera, Metti la cera, togli la cera, Metti la cera, togli la cera, Metti la cera, togli la cera";
+$lenght= strlen($testo);
+$testo_censurato= str_replace("cera", "xxx", $testo);
+$new_lenght= strlen($testo_censurato)
 ?>
 
 
@@ -13,8 +16,14 @@ $parola="popipopi";
     <title>Document</title>
 </head>
 <body>
-    <h1>
-        La parola del giorno è <?php echo $parola ?>
-    </h1>
+    
+    <h1>La frase del giorno è:</h1>
+    <h3> <?php echo $testo ?> </h3>
+    <p>Questa frase è lunga <?php echo $lenght ?> parole</p>
+
+    <h1>Abbiamo censurato tali amenità:</h1>
+    <h3> <?php echo $testo_censurato ?> </h3>
+    <p>Questa frase è lunga <?php echo $new_lenght ?> parole</p>
+
 </body>
 </html>
